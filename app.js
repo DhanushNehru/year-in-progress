86f0a766b6e93625c1810e67756d0c86e670c927
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-09-24 19:08:20 -0300

    Fixing api error

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const userLang = navigator.language || navigator.userLanguage;
-const langCode = userLang.split('-')[0];  // Extract language code
-const selectedLang = translations[langCode] ? langCode : 'en'; //'en' if unsupported
 
 app.get('/year-progress', (req, res) => {
+  const langCode = req.get('Accept-Language').split('-')[0] || "en";
+  console.log("Received lang param: " + langCode);
+
   const now = new Date();
   const currentYear = now.getFullYear();
   const start = new Date(currentYear, 0, 1);
   const end = new Date(currentYear + 1, 0, 1);
   const progress = ((now - start) / (end - start)) * 100;
   const progressFormatted = progress.toFixed(6);
-  
-  const svg = "";
-  switch (selectedLang){
-      case "es":
-        svg = `
-        <svg width="300" height="50" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100%" height="50" fill="#e0e0e0" />
-        <rect width="${progressFormatted}%" height="50" fill="#3b82f6" />
-        <text x="50%" y="30" alignment-baseline="middle" text-anchor="middle" fill="#000" font-size="12">
-          ${progressFormatted}% de ${currentYear} completado
-        </text>
-        </svg>
-        `;
-      break;
-      default:
-        svg = `
-        <svg width="300" height="50" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100%" height="50" fill="#e0e0e0" />
-        <rect width="${progressFormatted}%" height="50" fill="#3b82f6" />
-        <text x="50%" y="30" alignment-baseline="middle" text-anchor="middle" fill="#000" font-size="12">
-          ${progressFormatted}% of ${currentYear} completed
-        </text>
-        </svg>`;
-      break;
-  }
-  
+  let text = "";
+
+  if (langCode == 'es') text = `${progressFormatted}% de ${currentYear} completado`;
+  else text = `${progressFormatted}% of ${currentYear} completed`;
+
+  const svg = `
+    <svg width="300" height="50" xmlns="http://www.w3.org/2000/svg">
+    <rect width="100%" height="50" fill="#e0e0e0" />
+    <rect width="${progressFormatted}%" height="50" fill="#3b82f6" />
+    <text x="50%" y="30" alignment-baseline="middle" text-anchor="middle" fill="#000" font-size="12">
+    ${text}
+    </text>
+    </svg>`;
+      
   res.setHeader('Content-Type', 'image/svg+xml');
   res.send(svg);
 });
