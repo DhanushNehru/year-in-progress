940bba7d249d5439e5749560e39cae538adfb4a2
arya2004 <arya20j4@gmail.com>
2024-10-06 11:58:13 +0530

    refactor: Update static file paths in app.js and index.html

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/')));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.get('/year-progress', (req, res) => {
